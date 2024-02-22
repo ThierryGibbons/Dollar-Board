19196d24f24bba1cad5dabd69024394c60c3a02b
Thierry <270067227@yoobeestudent.ac.nz>
2024-02-22 13:32:06 +1300

    writes to json file

--- server/server.py
+++ server/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
+import os
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -11,17 +13,30 @@ def tempRoute():
         data = request.json
         # Process the data here
         print(data["message"])
-        match data["message"]:
-            case "Hello":
+        match data["message"].lower():
+            case "hello":
                 return {"name": "Server", "text": "Hello there"}
-            case "1":
-                return {"name": "Server", "text": "You selected 1"}
+            case "budget":
+                # Read the JSON file
+                with open("budget.json", "r") as file:
+                    data = json.load(file)
+                    # Convert data to string
+                    data_str = json.dumps(data)
+
+                # Put the data here
+                return {"name": "Server", "text": data_str}
             case "2":
                 return {"name": "Server", "text": "You selected 2"}
             case other:
                 return {"name": "Server", "text": "I don't understand"}
     else:
         return {"name": "Server", "text": "Hello from the server!"}
+
+# If there is no json file, create one
+if not os.path.isfile("budget.json"):
+    with open("budget.json", "w") as file:
+        file.write('{"House Bills": 0, "Personal Bills": 0, "Food": 0, "House Other": 0, "Entertainment": 0, "Savings": 0}')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
